2e0b6437fedee90a988c5443be4515ec01349119
wolfster1 <79382087+wolfster1@users.noreply.github.com>
2025-06-24 18:27:51 -0400

    Update sql_execution.py
    added code with requirements begging on like 28.

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -24,7 +24,57 @@ class QueryMixin:
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
     #### YOUR CODE HERE
+
+    #### updated code meeting requirements
+
+from sqlite3 import connect
+from pathlib import Path
+from functools import wraps
+import pandas as pd
+
+# Using pathlib, create a `db_path` variable
+# that points to the absolute path for the `employee_events.db` file
+db_path = Path(__file__).resolve().parents[1] / "employee_events.db"
+
+# OPTION 1: MIXIN
+# Define a class called `QueryMixin`
+class QueryMixin:
     
+    # Define a method named `pandas_query`
+    # that receives an SQL query as a string
+    # and returns the query's result as a pandas dataframe
+    def pandas_query(self, sql_query):
+        connection = connect(db_path)
+        df = pd.read_sql_query(sql_query, connection)
+        connection.close()
+        return df
+
+    # Define a method named `query`
+    # that receives an sql_query as a string
+    # and returns the query's result as a list of tuples
+    def query(self, sql_query):
+        connection = connect(db_path)
+        cursor = connection.cursor()
+        result = cursor.execute(sql_query).fetchall()
+        connection.close()
+        return result
+
+# Leave this code unchanged
+def query(func):
+    """
+    Decorator that runs a standard sql execution
+    and returns a list of tuples
+    """
+    @wraps(func)
+    def run_query(*args, **kwargs):
+        query_string = func(*args, **kwargs)
+        connection = connect(db_path)
+        cursor = connection.cursor()
+        result = cursor.execute(query_string).fetchall()
+        connection.close()
+        return result
+    
+    return run_query
 
  
  # Leave this code unchanged
