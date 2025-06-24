a843b868ae6f0d419bceff70a14c1231988039df
wolfster1 <79382087+wolfster1@users.noreply.github.com>
2025-06-24 18:25:40 -0400

    Update query_base.py
    added code beginning at line 51.

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -48,3 +48,52 @@
         # for the table name in the `name` class attribute
         # YOUR CODE HERE
 
+#### Code inserted with requirements from above below
+
+# Import any dependencies needed to execute SQL queries
+import pandas as pd
+from report.src.sql_execution import execute_dataframe
+
+# Define a class called QueryBase
+# Use inheritance to add methods for querying the employee_events database.
+class QueryBase:
+    
+    # Create a class attribute called `name`
+    # Set the attribute to an empty string
+    name = ""
+
+    # Define a `names` method that receives no passed arguments
+    def names(self):
+        # Return an empty list
+        return []
+
+    # Define an `event_counts` method that receives an `id` argument
+    # This method should return a pandas DataFrame
+    def event_counts(self, id):
+        # QUERY 1
+        query = f"""
+            SELECT event_date,
+                   SUM(positive_events) AS positive_events,
+                   SUM(negative_events) AS negative_events
+            FROM {self.name}
+            JOIN employee_events
+                USING({self.name}_id)
+            WHERE {self.name}_id = {id}
+            GROUP BY event_date
+            ORDER BY event_date
+        """
+        return execute_dataframe(query)
+
+    # Define a `notes` method that receives an id argument
+    # This function should return a pandas DataFrame
+    def notes(self, id):
+        # QUERY 2
+        query = f"""
+            SELECT note_date, note
+            FROM notes
+            JOIN {self.name}
+                USING({self.name}_id)
+            WHERE {self.name}_id = {id}
+        """
+        return execute_dataframe(query)
+
