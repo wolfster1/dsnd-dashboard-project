219fcb9c7a7597db5bc27ceb8376a0c03b1ea687
wolfster1 <79382087+wolfster1@users.noreply.github.com>
2025-06-24 18:23:17 -0400

    Update employee.py
    added code beginning at line 67 for employee.py

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -63,3 +63,56 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+
+#### Project code is below with requirements 
+
+# Import the QueryBase class
+from report.src.sql_base import QueryBase
+
+# Import dependencies needed for SQL execution
+# from the `sql_execution` module
+from report.src.sql_execution import execute_query, execute_dataframe
+
+# Define a subclass of QueryBase called Employee
+class Employee(QueryBase):
+
+    # Set the class attribute `name` to the string "employee"
+    name = "employee"
+
+    # Define a method called `names` that receives no arguments
+    # and returns a list of tuples from an SQL execution
+    def names(self):
+        # Query 3
+        # Write an SQL query that selects two columns:
+        # 1. The employee's full name
+        # 2. The employee's id
+        query = f"""
+            SELECT full_name, {self.name}_id
+            FROM {self.name}
+        """
+        return execute_query(query)
+
+    # Define a method called `username` that receives an `id` argument
+    # and returns a list of tuples from an SQL execution
+    def username(self, id):
+        # Query 4
+        # Write an SQL query that selects an employee's full name
+        # where the employee ID matches the given `id`
+        query = f"""
+            SELECT full_name
+            FROM {self.name}
+            WHERE {self.name}_id = {id}
+        """
+        return execute_query(query)
+
+    # Return a pandas DataFrame from the executed SQL query
+    def model_data(self, id):
+        query = f"""
+            SELECT SUM(positive_events) AS positive_events,
+                   SUM(negative_events) AS negative_events
+            FROM {self.name}
+            JOIN employee_events
+                USING({self.name}_id)
+            WHERE {self.name}_id = {id}
+        """
+        return execute_dataframe(query)
